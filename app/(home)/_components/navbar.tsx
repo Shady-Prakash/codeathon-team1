07940de4a4f2349417272564650d0eadd12ea1d9
nataliiazab <113237479+nataliiazab@users.noreply.github.com>
2024-09-13 17:21:12 +0100

    Refactor:update yarn.lock and fix errors in navbar by ensuring that window runs only in the browser

--- app/(home)/_components/navbar.tsx
+++ app/(home)/_components/navbar.tsx
@@ -1,37 +1,38 @@
-"use client"
-import Link from "next/link"
-import { Menu, X} from "lucide-react"
-import { useEffect, useState } from "react"
-import Image from "next/image"
-import { Button } from "@/components/ui/button"
-import Donate from "./paypal/donate"
-import { useRouter } from "next/navigation"
-import {useAuth, UserButton } from "@clerk/nextjs"
-import { Actions } from "./actions"
+"use client";
+
+import Link from "next/link";
+import { Menu, X } from "lucide-react";
+import { useEffect, useState } from "react";
+import Image from "next/image";
+import { Button } from "@/components/ui/button";
+import Donate from "./paypal/donate";
+import { useRouter } from "next/navigation";
+import { useAuth, UserButton } from "@clerk/nextjs";
+import { Actions } from "./actions";
 
 const NavBar = () => {
   const [state, setState] = useState(false);
-  const [prevScrollpos, setPrevScrollpos] = useState(window.scrollY);
+  const [prevScrollpos, setPrevScrollpos] = useState(0);
   const [top, setTop] = useState(0);
   const router = useRouter();
-  const {userId} = useAuth()
+  const { userId } = useAuth();
 
   useEffect(() => {
-    // Function to handle scroll events
     const handleScroll = () => {
       const currentScrollPos = window.scrollY;
       if (prevScrollpos > currentScrollPos) {
-        setTop(0); // Show navbar
+        setTop(0); 
       } else {
-        setTop(-110); // Hide navbar
+        setTop(-110);
       }
       setPrevScrollpos(currentScrollPos);
     };
-    // Add scroll event listener when the component mounts
-    window.addEventListener('scroll', handleScroll);
-    // Clean up by removing the event listener when the component unmounts
+
+
+    window.addEventListener("scroll", handleScroll);
+
     return () => {
-      window.removeEventListener('scroll', handleScroll);
+      window.removeEventListener("scroll", handleScroll);
     };
   }, [prevScrollpos]);
 
@@ -42,38 +43,43 @@ const NavBar = () => {
     { title: "Stories", path: "/stories" },
     { title: "Work with Us", path: "/work" },
     { title: "Contact Us", path: "/contact-us" },
-  ]
+  ];
 
   const clickHandler = () => {
-    router.push('/sign-in')
-  }
+    router.push("/sign-in");
+  };
 
   const donationHandler = async () => {
-  const response = await fetch("https://www.paypal.com/donate", {
+    await fetch("https://www.paypal.com/donate", {
       method: "POST",
     });
-  }
+  };
 
   return (
-    
-    <nav 
-      className ={!state ? `sticky top-${top} bg-white z-10 w-full border-b md:border-0` : "fixed bg-white z-10 w-full border-b md:border-0"} style={{transition: 'top ease-in-out 0.3s'}}>
+    <nav
+      className={`${
+        !state
+          ? `sticky top-${top} bg-white z-10 w-full border-b md:border-0`
+          : "fixed bg-white z-10 w-full border-b md:border-0"
+      }`}
+      style={{ transition: "top ease-in-out 0.3s" }}
+    >
       <div className="items-center px-4 max-w-screen-2xl mx-auto md:flex md:px-8">
         <div className="flex items-center justify-between py-3 md:py-5 md:block">
           <Link href="/">
-          <Image
-            src="/assets/logo.png"
-            width={70}
-            height={70}
-            alt="Branding logo"
-          />
+            <Image
+              src="/assets/logo.png"
+              width={70}
+              height={70}
+              alt="Branding logo"
+            />
           </Link>
           <div className="md:hidden">
             <button
               className="text-gray-700 outline-none p-2 rounded-md focus:border-gray-400 focus:border"
               onClick={() => setState(!state)}
             >
-              {!state ? <Menu/> : <X/>}
+              {!state ? <Menu /> : <X />}
             </button>
           </div>
         </div>
@@ -84,32 +90,36 @@ const NavBar = () => {
         >
           <ul className="justify-center items-center space-y-5 md:flex md:space-x-10 md:space-y-0">
             {menus.map((item, idx) => (
-              <li key={idx} className="font-medium text-black hover:text-indigo-600">
+              <li
+                key={idx}
+                className="font-medium text-black hover:text-indigo-600"
+              >
                 <Link href={item.path}>{item.title}</Link>
               </li>
             ))}
           </ul>
         </div>
-          <div className={`${state ? "block text-center" : "hidden"} md:flex gap-3 items-center`}>
-            {
-              !userId 
-              ? <Button variant="success" border="rounded" size="lg" onClick={clickHandler}>Sign in</Button> 
-              : 
-              // <form action="https://www.paypal.com/donate" method="post" target="_top">
-              //   <input type="hidden" name="hosted_button_id" value="C63JQ5E9GLUSC" />
-                  <Actions
-                // userId={row.original.publicUserData?.userId}
-                // role={row.original.role}
-              />
-              // </form>
-                  }
-              {/* <UserButton afterSignOutUrl="/"/> */}
-          {/* <Donate/> */}
-
+        <div
+          className={`${
+            state ? "block text-center" : "hidden"
+          } md:flex gap-3 items-center`}
+        >
+          {!userId ? (
+            <Button
+              variant="success"
+              border="rounded"
+              size="lg"
+              onClick={clickHandler}
+            >
+              Sign in
+            </Button>
+          ) : (
+            <Actions />
+          )}
         </div>
       </div>
     </nav>
-  )
-}
+  );
+};
 
-export default NavBar
+export default NavBar;
--- next.config.js
+++ next.config.js
@@ -1,10 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: [
-      "utfs.io"
-    ]
-  }
-}
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "utfs.io",
+        pathname: "/**",
+      },
+    ],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
