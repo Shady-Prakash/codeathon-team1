161125556729b38944dbd5ec5952b5f6518bcf5d
Olena Reukova <kronverk01@gmail.com>
2024-09-14 15:54:27 +0100

    component:home-page

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,13 +1,15 @@
-import NavBar from "./_components/navbar"
-import Slider from "./_components/slider"
+import NavBar from './_components/navbar';
+import Slider from './_components/slider';
+import Campain from './_components/campaign';
 
 const page = () => {
   return (
     <>
-      <NavBar/>
-      <Slider/>
+      <NavBar />
+      <Slider />
+      <Campain />
     </>
-  )
-}
+  );
+};
 
-export default page
+export default page;
