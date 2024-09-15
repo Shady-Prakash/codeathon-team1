4706cb0a323f5d5b628421e3b121c9dd7d7cfd07
Farzaneh Haghani <farzaneh.haghani@students.codeyourfuture.io>
2024-09-15 21:58:51 +0100

    logo & category fixed

--- app/dashboard/(dashboard)/_components/logo.tsx
+++ app/dashboard/(dashboard)/_components/logo.tsx
@@ -1,12 +1,15 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export const Logo = () => {
   return (
-    <Image
-      height={130}
-      width={130}
-      alt="logo"
-      src="/logo.svg"
-    />
-  )
-}
+    <Link href="/">
+      <Image
+        src="/assets/logo.png"
+        width={70}
+        height={70}
+        alt="Branding logo"
+      />
+    </Link>
+  );
+};
