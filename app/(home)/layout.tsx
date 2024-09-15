7382eac0411fcbe236bcba0a01d2060e892b5739
Pavan <73477406+Pavan393a@users.noreply.github.com>
2024-09-15 18:30:50 +0000

    footer added to the bottom

--- app/(home)/layout.tsx
+++ app/(home)/layout.tsx
@@ -1,5 +1,5 @@
-import NavBar from './_components/navbar';
-import Footer from './_components/footer';
+import NavBar from "./_components/navbar";
+import Footer from "./_components/footer";
 
 const HomeLayout = ({ children }: { children: React.ReactNode }) => {
   return (
