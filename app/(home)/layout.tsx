f67f77f02fd1f9d2f6b19be23b7f0f222a14dc10
Pavan <73477406+Pavan393a@users.noreply.github.com>
2024-09-15 19:14:28 +0000

    updated footer to the bottom

--- app/(home)/layout.tsx
+++ app/(home)/layout.tsx
@@ -3,11 +3,11 @@ import Footer from "./_components/footer";
 
 const HomeLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <>
+    <div className="flex flex-col min-h-screen">
       <NavBar />
-      <main>{children}</main>
+      <main className="flex-grow">{children}</main>
       <Footer />
-    </>
+    </div>
   );
 };
 
