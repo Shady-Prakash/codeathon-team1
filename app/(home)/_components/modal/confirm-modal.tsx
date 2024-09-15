0e98d83e2502b35da2612186c559a6ba87251503
Pavan <73477406+Pavan393a@users.noreply.github.com>
2024-09-15 14:11:21 +0000

    added both userpage and company page

--- app/(home)/_components/modal/confirm-modal.tsx
+++ app/(home)/_components/modal/confirm-modal.tsx
@@ -9,33 +9,36 @@ import {
   AlertDialogFooter,
   AlertDialogHeader,
   AlertDialogTitle,
-  AlertDialogTrigger
-} from "@/components/ui/alert-dialog"
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog";
+import { useRouter } from "next/navigation";
 
 interface ConfirmModalProps {
   children: React.ReactNode;
   onConfirm: () => void;
-};
+}
 
-export const ConfirmModal = ({
-  children,
-  onConfirm
-}: ConfirmModalProps) => {
+export const ConfirmModal = ({ children, onConfirm }: ConfirmModalProps) => {
+  const router = useRouter();
+  const handleClickUser = () => {
+    router.push("/donation/userpage");
+    onConfirm();
+  };
+  const handleClickCompany = () => {
+    router.push("/donation/companypage");
+    onConfirm();
+  };
   return (
     <AlertDialog>
-      <AlertDialogTrigger asChild>
-        {children}
-      </AlertDialogTrigger>
+      <AlertDialogTrigger asChild>{children}</AlertDialogTrigger>
       <AlertDialogContent>
         <AlertDialogHeader>
           <AlertDialogTitle>Register first</AlertDialogTitle>
-          <AlertDialogDescription>
-            Want to new donor as
-          </AlertDialogDescription>
+          <AlertDialogDescription>Want to new donor as</AlertDialogDescription>
         </AlertDialogHeader>
         <AlertDialogFooter>
-          <AlertDialogCancel>Solo</AlertDialogCancel>
-          <AlertDialogAction onClick={onConfirm}>
+          <AlertDialogCancel onClick={handleClickUser}>Solo</AlertDialogCancel>
+          <AlertDialogAction onClick={handleClickCompany}>
             Team
           </AlertDialogAction>
         </AlertDialogFooter>
