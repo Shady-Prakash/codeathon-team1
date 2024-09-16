959149db81beee5e975fda15cb06b7f24613eacb
Farzaneh Haghani <farzaneh.haghani@students.codeyourfuture.io>
2024-09-16 11:58:48 +0100

    minor changed

--- app/dashboard/(dashboard)/(routes)/admin/campaigns/[campaignId]/_components/category-form.tsx
+++ app/dashboard/(dashboard)/(routes)/admin/campaigns/[campaignId]/_components/category-form.tsx
@@ -3,7 +3,7 @@
 import * as z from "zod";
 import axios from "axios";
 import { zodResolver } from "@hookform/resolvers/zod";
-import {useForm} from "react-hook-form";
+import { useForm } from "react-hook-form";
 import { Pencil } from "lucide-react";
 import { useState } from "react";
 import { Campaign } from "@prisma/client";
@@ -13,7 +13,7 @@ import {
   FormControl,
   FormField,
   FormItem,
-  FormMessage
+  FormMessage,
 } from "@/components/ui/form";
 import { Button } from "@/components/ui/button";
 import toast from "react-hot-toast";
@@ -22,16 +22,15 @@ import { cn } from "@/lib/utils";
 import { Textarea } from "@/components/ui/textarea";
 import { Combobox } from "@/components/ui/combobox";
 
-
 interface CategoryFormProps {
   initialData: Campaign;
   campaignId: string;
-  options: { label: string; value: string; }[];
+  options: { label: string; value: string }[];
 }
 
 const formSchema = z.object({
- categoryId: z.string().min(1),
-})
+  categoryId: z.string().min(1),
+});
 
 export const CategoryForm = ({
   initialData,
@@ -47,13 +46,13 @@ export const CategoryForm = ({
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
-      categoryId: initialData?.categoryId || ""
+      categoryId: initialData?.categoryId || "",
     },
   });
 
   const { isSubmitting, isValid } = form.formState;
 
-  const onSubmit = async(values: z.infer<typeof formSchema>) => {
+  const onSubmit = async (values: z.infer<typeof formSchema>) => {
     try {
       await axios.patch(`/api/campaigns/${campaignId}`, values);
       toast.success("Category updated");
@@ -62,33 +61,43 @@ export const CategoryForm = ({
     } catch {
       toast.error("Something went wrong");
     }
-  }
+  };
 
-  const selectedOption = options.find((option) => option.value === initialData.categoryId);
+  const selectedOption = options.find(
+    (option) => option.value === initialData.categoryId
+  );
 
   return (
     <div className="mt-6 border bg-slate-100 rounded-md p-4">
       <div className="font-medium flex items-center justify-between">
-        Course category
+        Campaign category
         <Button onClick={toggleEdit} variant="ghost">
           {isEditing ? (
             <>Cancel</>
           ) : (
             <>
-              <Pencil className="h-4 w-4 mr-2"/>
+              <Pencil className="h-4 w-4 mr-2" />
               Edit category
             </>
           )}
         </Button>
       </div>
       {!isEditing && (
-        <p className={cn("text-sm mt-2", !initialData.categoryId && "text-slate-500 italic")}>
+        <p
+          className={cn(
+            "text-sm mt-2",
+            !initialData.categoryId && "text-slate-500 italic"
+          )}
+        >
           {selectedOption?.label || "No category"}
         </p>
       )}
       {isEditing && (
         <Form {...form}>
-          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4 mt-4">
+          <form
+            onSubmit={form.handleSubmit(onSubmit)}
+            className="space-y-4 mt-4"
+          >
             <FormField
               control={form.control}
               name="categoryId"
@@ -97,18 +106,16 @@ export const CategoryForm = ({
                   <FormControl>
                     <Combobox
                       options={options}
-                      value= {field.value}
+                      value={field.value}
                       onChange={(value) => field.onChange(value)}
                     />
                   </FormControl>
-                  <FormMessage/>
+                  <FormMessage />
                 </FormItem>
               )}
             />
             <div className="flex items-center gap-x-2">
-              <Button
-                disabled={!isValid || isSubmitting}
-                type="submit">
+              <Button disabled={!isValid || isSubmitting} type="submit">
                 Save
               </Button>
             </div>
@@ -116,5 +123,5 @@ export const CategoryForm = ({
         </Form>
       )}
     </div>
-  )
-}
+  );
+};
