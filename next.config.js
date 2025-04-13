1a2d42633ba4949370b6db80a47be2b600d8a4fa
Shady-Prakash <shadyprakash8@gmail.com>
2025-04-13 10:32:57 +0100

    reset name to js file

--- next.config.js
+++ next.config.js
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   typescript: {
-    ignoreBuildErrors: true
+    ignoreBuildErrors: true,
+    ignoreDuringBuilds: true,
   },
   eslint: {
     ignoreDuringBuilds: true
@@ -12,3 +13,5 @@ const nextConfig = {
     ]
   }
 }
+
+module.exports = nextConfig
